025856990158271582e1f75a5bc3be2675da434d
Askorbinochka <tetyana.maletska@plp-lp.com.ua>
2026-09-10 11:36:50 +0300

    add classes weapons

---
+++ Weapons.cs
@@ -0,0 +1,37 @@
+﻿using Project;
+using System;
+
+namespace Project
+{
+    public class Sword : IWeapon
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Удар мечем!");
+        }
+    }
+
+    public class Bow : IWeapon
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Постріл з лука!");
+        }
+    }
+
+    public class Axe : IWeapon
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Удар сокирою!");
+        }
+    }
+
+    public class Knife : IWeapon
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Удар ножем!");
+        }
+    }
+}
