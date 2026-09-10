bcb98d5cf9f7a5770814849156b6601706a9c994
Askorbinochka <tetyana.maletska@plp-lp.com.ua>
2026-09-10 11:40:06 +0300

    create class player

---
+++ Player.cs
@@ -0,0 +1,17 @@
+﻿using Project;
+using System;
+
+namespace Project1
+{
+    public abstract class Player
+    {
+        protected IWeapon weapon;
+
+        public void SetWeapon(IWeapon w)
+        {
+            this.weapon = w;
+        }
+
+        public abstract void Display();
+    }
+}
